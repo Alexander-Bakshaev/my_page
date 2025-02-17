02274d99d284a476b6341d74b4d17c38a98691db
Alexander-Bakshaev <shuriknt@gmail.com>
2025-02-17 16:07:32 +0500

    Добавлен redirect 2

--- calculate_geometry/views.py
+++ calculate_geometry/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseNotFound
 
 def get_rectangle_area(request, width: int, height: int) -> HttpResponse:
     return HttpResponse(f"Площадь прямоугольника {width} * {height} = {width * height}")
@@ -9,3 +9,14 @@ def get_square_area(request, width: int) -> HttpResponse:
 def get_circle_area(request, radius: int) -> HttpResponse:
     return HttpResponse(f"Площадь круга {radius} * {radius} * {3.14} = {radius * radius * 3.14}")
 
+def get_rectangle_area_redirect(request, width: int, height: int) -> HttpResponse:
+    """Перенаправление на rectangle"""
+    return HttpResponseRedirect(f"/calculate_geometry/rectangle/{width}/{height}")
+
+def get_square_area_redirect(request, width: int) -> HttpResponse:
+    """Перенаправление на square"""
+    return HttpResponseRedirect(f"/calculate_geometry/square/{width}")
+
+def get_circle_area_redirect(request, radius: int) -> HttpResponse:
+    """Перенаправление на circle"""
+    return HttpResponseRedirect(f"/calculate_geometry/circle/{radius}")
